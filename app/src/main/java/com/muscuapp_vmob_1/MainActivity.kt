6f317c172a455a4f106ecf21a105458d7b385c2b
Antoine Gaillard <antoine.gaillard2608@gmail.com>
2026-04-21 01:20:49 -0400

    début notif *

--- app/src/main/java/com/muscuapp_vmob_1/MainActivity.kt
+++ app/src/main/java/com/muscuapp_vmob_1/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.muscuapp_vmob_1
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.room.Room
 import com.muscuapp_vmob_1.data.source.MuscuDataBase
@@ -24,8 +28,20 @@ class MainActivity : ComponentActivity() {
             .fallbackToDestructiveMigration()
             .build()
     }
+
+    //ici je me suis documenté sur la doc Android puis j'ai utilisé les fonctionnalités de l'IDE pour compléter
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val channel = NotificationChannel(
+            "channel_id",
+            "Time to Train",
+            NotificationManager.IMPORTANCE_DEFAULT,
+        ).apply {
+            description = "It's 12p.m. - Time to train !!!"
+        }
+        val manager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        manager.createNotificationChannel(channel)
         enableEdgeToEdge()
         setContent {
             val isDark = isSystemInDarkTheme()
